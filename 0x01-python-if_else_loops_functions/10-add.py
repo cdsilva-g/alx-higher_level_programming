6cc7e96967afa4b09a9f48a2567343883196fe26
cdsilva-g <christinegathuu@gmail.com>
2023-03-08 01:42:45 +0300

    Tasks

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def add(a, b):
+    # function adds a to b
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def pow(a, b):
+    # function computes a to power b
+    return (a ** b)
