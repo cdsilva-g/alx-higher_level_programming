d1f08e1f2b73fe8efe04dd9fe06181abc86696f7
cdsilva-g <christinegathuu@gmail.com>
2023-03-13 14:57:40 +0300

    Task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for i in my_list[i]:
+        idx = i
+        if idx < 0:
+            return None
+        elif idx > len(my_list):
+            return None
+        else:
+            return idx
